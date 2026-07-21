2f9259de7d16d4aee85b3c4f9bddc7d9d55c5b61
Rahul Bhadoriya <rahulbh8077@gmail.com>
2026-07-22 00:26:41 +0530

    Pattern Printing Continues-3
    Implement a function to print a number pattern based on user input.

---
+++ Pattern_3.cpp
@@ -0,0 +1,25 @@
+
+#include<iostream>
+using namespace std;
+
+ void pattern3(int n) {
+    for(int i=1;i<n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+    
+}
+int main()
+{
+    int n;
+    cout<<"Enter the no of inputs please to run smooothly";
+    cin>>n;
+    pattern3(n);
+    return 0;
+}
+// 1
+// 22
+// 333
+// 4444
