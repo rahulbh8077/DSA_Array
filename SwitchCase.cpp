3c4335971e3eb11cea6520076a83e6d2b114ec4d
Rahul Bhadoriya <rahulbh8077@gmail.com>
2026-07-23 00:47:28 +0530

    Switch Case Implementaion
    Implement switch case for day of the week.

---
+++ SwitchCase.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h> //all files are included in this libraruy.
+#include<iostream>
+using namespace std;
+ int main(){
+    int day;
+    cin>> day;
+    switch(day){
+        case 1:
+        cout<< "Monday";
+        break;
+        case 2:
+        cout<< "Tuesday";
+        break;
+        case 3:
+        cout<< "Wed";
+        break;
+        case 4:
+        cout<< "Thurs";
+        break;
+        case 5:
+        cout<< "Fri";
+        break;
+        case 6:
+        cout<< "Sat";
+        break;
+        case 7:
+        cout<< "Sun";
+        break;
+     // more cases as per the requiremnet.
+     default:
+     cout<<"enter a valid input please";
+    }
+    
+    
+    
+    return 0;
+
+}
